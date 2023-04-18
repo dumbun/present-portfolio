05b61848ecb28f5ecaaab861ee7f6d8826f93f46
vamshi krishna <113350510+dumbun@users.noreply.github.com>
2023-04-18 12:40:39 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,7 +80,7 @@ export default function Home() {
             <Image
               alt="Vamshi Krishna"
               src={dumbun}
-              layout="fill"
+              layout="fit"
               objectFit="cover"
             />
           </div>
